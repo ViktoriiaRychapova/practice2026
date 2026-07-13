0c1b2046eda5852c1e60b8eaf9252b6ab57f4d03
ViktoriiaRychapova <viktoriiarychapova@gmail.com>
2026-07-13 18:44:41 +0600

    task14

---
+++ task14/DefiniteIntegral.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace task14
+{
+    public class DefiniteIntegral
+    {
+        private static object lockObj = new object();
+
+        public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+        {
+            double totalResult = 0.0;
+            Barrier barrier = new Barrier(threadsNumber);
+            Thread[] threads = new Thread[threadsNumber];
+
+            double range = b - a;
+            double segmentSize = range / threadsNumber;
+
+            for (int i = 0; i < threadsNumber; i++)
+            {
+                int threadIndex = i;
+                threads[i] = new Thread(() =>
+                {
+                    double localA = a + threadIndex * segmentSize;
+                    double localB = localA + segmentSize;
+
+                    double localSum = 0.0;
+
+                    for (double x = localA; x < localB; x += step)
+                    {
+                        double fx = function(x);
+                        double fxNext = function(x + step);
+                        localSum += (fx + fxNext) * 0.5 * step;
+                    }
+
+                    lock (lockObj)
+                    {
+                        totalResult += localSum;
+                    }
+
+                    barrier.SignalAndWait();
+                });
+                threads[i].IsBackground = true;
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return totalResult;
+        }
+    }
+}
---
+++ task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using task14;
+using System;
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void Test_Solve_Linear()
+    {
+        Func<double, double> X = x => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void Test_Solve_Sin()
+    {
+        Func<double, double> SIN = x => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void Test_Solve_Constant()
+    {
+        Func<double, double> X = x => 1;
+        Assert.Equal(5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
